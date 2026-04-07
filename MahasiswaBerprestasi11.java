36cb81fb02d8e703e8be0b823159774ac011b4bd
firdauzynuzula <nuzulafirdhauzy@gmail.com>
2026-04-07 09:26:20 +0700

    edit file MahasiswaBerprestasi11.java

--- MahasiswaBerprestasi11.java
+++ MahasiswaBerprestasi11.java
@@ -47,34 +47,4 @@ public class MahasiswaBerprestasi11 extends Mahasiswa11 {
         System.out.println("Nilai Prestasi: " + nilaiPrestasi);
         System.out.println("Beasiswa: " + beasiswa);
     }
-    
-    // Override metode tampilkanInfoLengkap
-    @Override
-    public void tampilkanInfoLengkap() {
-        tampilkanInfo();
-        System.out.println("Jumlah Penghargaan: " + jumlahPenghargaan);
-        if (jumlahPenghargaan > 0) {
-            System.out.println("Daftar Penghargaan:");
-            for (int i = 0; i < jumlahPenghargaan; i++) {
-                System.out.println("  " + (i + 1) + ". " + penghargaan[i]);
-            }
-        }
-        System.out.println("Skor Prestasi: " + String.format("%.2f", hitungSkorPrestasi()));
-        System.out.println("Kategori: " + (adalahBerprestasi() ? "Berprestasi" : "Reguler"));
-        System.out.println("Siap Lulus: " + siapLulus());
-    }
-    
-    // Override metode toString
-    @Override
-    public String toString() {
-        return "MahasiswaBerprestasi11{" +
-                "nama='" + nama + '\'' +
-                ", nim='" + nim + '\'' +
-                ", nilaiIpk=" + nilaiIpk +
-                ", kelas='" + kelas + '\'' +
-                ", nilaiPrestasi=" + nilaiPrestasi +
-                ", beasiswa='" + beasiswa + '\'' +
-                ", jumlahPenghargaan=" + jumlahPenghargaan +
-                '}';
-    }
 }
